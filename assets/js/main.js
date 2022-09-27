71a180abc09ffb288c6dd006a75177a0938d13a8
Kamal Eladly <kamaleladly90@gmail.com>
2022-09-27 13:57:20 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+
+$('.buttons button').click(function(){
+  $(this).addClass('active');
+  $(this).siblings().removeClass('active');
+  btnId=$(this).attr('id');
+  if($(this).eq(0)){
+    $(`.portfolio .content .row >div`).fadeIn();
+
+  }
+  $(`.portfolio .content .row >div[custom=${btnId}]`).siblings().css("display","none");
+$(`.portfolio .content .row >div[custom=${btnId}]`).css("display",'block');
+
+})
+
+
+$(window).scroll(function(){
+
+    if($(this).scrollTop()> $('#services').offset().top-100){
+    
+        $("#nav").css({
+          backgroundColor:"white",
+        })
+
+        $('.navlink').css("color","black")
+        $('.btn-nav').css("color","black")
+
+    }
+    else{
+      $("#nav").css('backgroundColor',"transparent")
+      $('.navlink').css("color","white")
+      $('.btn-nav').css("color","white")
+
+    }
+})
+
+
+let input=document.getElementsByClassName('form-control')
+console.log(input);
+
+for(let i=0;i<input.length;i++){
+  input[i].addEventListener('focus',function(){
+    this.style.borderBottom="2px solid #fa450e";
+  });
+}
+
+for(let i=0;i<input.length;i++){
+  input[i].addEventListener('blur',function(){
+    this.style.borderBottom="2px solid white";
+  });
+}
